5966f1c4d9417013d4c347f80b99d4122f7ddc7c
Sumeet Kumar <81513463+sumeetk10@users.noreply.github.com>
2024-02-04 00:13:23 +0530

    initial commit

---
+++ src/components/UI/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './Footer.css';
+
+const footer = (props) => (
+    <div className={classes.Footer}>
+      <p className={classes.Text}>Burger Bazaar: One stop shop for all your needs</p>  
+    </div>
+);
+
+export default footer;
